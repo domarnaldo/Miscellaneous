0a25b10986a3428182a4a66d9c8f0f825a66c62c
Dominick Arnaldo <darideem@gmail.com>
2012-07-19 19:31:08 -0400

    Added more filtering

--- getByClassName/src.js
+++ getByClassName/src.js
@@ -1,13 +1,20 @@
 /*
-*getByClassName - Written by DominickA
-*https://github.com/DominickA/Miscellaneous/blob/master/getByClassName/
-*/
-(function () {
-    var getByClassName = function (cname, tag) {
+ *getByClassName - Written by DominickA
+ *https://github.com/DominickA/Miscellaneous/blob/master/getByClassName/
+ */ 
+ (function () {
+    var getByClassName = function (cname, tag, direct) {
             if (document.getElementsByClassName) { //Check to see if the browser supports document.getElementsByClassName
                 if (tag != undefined) {
-                    var tag_name = document.getElementsByTagName(tag);
-                    for (i = 0; i < tag_name.length; i++) return tag_name[i].getElementsByClassName(cname); //Perform another loop to find the tag names
+                    var tag_name = document.getElementsByTagName(tag),
+                        ele_array = [];
+                    for (i = 0; i < tag_name.length; i++) {
+                        if (direct == true) {
+                            var search = new RegExp("(^|\\s)" + cname + "(\\s|$)");
+                            if (search.test(tag_name[i].className)) ele_array.push(tag_name[i]);
+                        } else return tag_name[i].getElementsByClassName(cname); //Perform another loop to find the tag names
+                    }
+                    return ele_array;
                 } else return document.getElementsByClassName(cname); //If there's no tag defined, return the default browser's function
             } else { //All older browsers
                 if (tag != undefined) var elements = document.getElementsByTagName(tag);
@@ -19,9 +26,13 @@
                 for (i = 0; i < elements.length; i++) {
                     var search = new RegExp("(^|\\s)" + cname + "(\\s|$)"); //RegExp to split multiple class names for testing
                     if (tag != undefined) {
-                        var tag_parent = elements[i].childNodes;
-                        for (x = 0; x < tag_parent.length; x++) { //Another for loop to test all of the element's child nodes
-                            if (search.test(tag_parent[x].className)) ele_array.push(tag_parent[x]); //Test if the class name equals the class name to look for and put those elements into the empty array
+                        if (direct == true) {
+                            if (search.test(elements.className)) ele_array.push(elements[i]);
+                        } else {
+                            var tag_parent = elements[i].childNodes;
+                            for (x = 0; x < tag_parent.length; x++) { //Another for loop to test all of the element's child nodes
+                                if (search.test(tag_parent[x].className)) ele_array.push(tag_parent[x]); //Test if the class name equals the class name to look for and put those elements into the empty array
+                            }
                         }
                     } else {
                         if (search.test(elements[i].className)) ele_array.push(elements[i]); //If no tag name is defined, return the elements that have the class name
